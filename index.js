51df827d7d51362153a147096ae85c890b8ad3d8
kurnia septia wiguna <kurniaseptiawiguna9@gmail.com>
2022-01-17 14:53:46 +0700

    done

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./src/routes")
 const app = express();
-// const port = 5000;
+const port = 5000;
 app.use(express.json());
 app.use(cors());
 app.use('/api',router)
 require('./src/config/db')
-app.listen(process.env.PORT || 5000, () => console.log(`Listening on port ${port}`))
+app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`))
